6a8d9ff44240f40bc811f72378be2fbb14dc1700
Kevin J. Rosario <kevinjooo59@gmail.com>
2025-01-16 15:34:54 -0400

    update some content

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,6 +4,29 @@
 
 $(document).ready(() => {
 
+    $(function () {
+        $('.btn-6')
+            .on('mouseenter', function (e) {
+                var parentOffset = $(this).offset(),
+                    relX = e.pageX - parentOffset.left,
+                    relY = e.pageY - parentOffset.top;
+                $(this).find('span').css({
+                    top: relY,
+                    left: relX
+                })
+            })
+            .on('mouseout', function (e) {
+                var parentOffset = $(this).offset(),
+                    relX = e.pageX - parentOffset.left,
+                    relY = e.pageY - parentOffset.top;
+                $(this).find('span').css({
+                    top: relY,
+                    left: relX
+                })
+            });
+    });
+
+
     $("body").click(() => {
         if (show) {
             show = openCloseMenu();
@@ -38,31 +61,31 @@ $(document).ready(() => {
 
     $(".nav-link").click((e) => {
 
-        var n = e.target.href.split("#");
-        let navLink = "#" + n[n.length - 1];
-        console.log(e.target.id)
-        // var navList = document.querySelectorAll(".nav-link");
-        // console.log(navList)
-        $(".nav-link").removeClass("current");
-        $("#" + e.target.id).addClass("current");
+            var n = e.target.href.split("#");
+            let navLink = "#" + n[n.length - 1];
+            console.log(e.target.id)
+            // var navList = document.querySelectorAll(".nav-link");
+            // console.log(navList)
+            $(".nav-link").removeClass("current");
+            $("#" + e.target.id).addClass("current");
 
-        // for (var p in navList) {
+            // for (var p in navList) {
 
-        //     console.log(p)
-        // }
-
-
-        var offsetTop = $(navLink).offset().top;
+            //     console.log(p)
+            // }
 
 
-        if (offsetTop || offsetTop == 0) {
-            $('html,body').animate({
-                scrollTop: offsetTop
-            }, 1000);
+            var offsetTop = $(navLink).offset().top;
+
+
+            if (offsetTop || offsetTop == 0) {
+                $('html,body').animate({
+                    scrollTop: offsetTop
+                }, 1000);
+            }
+            show = openCloseMenu()
+
         }
-        show = openCloseMenu()
-
-    }
 
     );
 
@@ -70,7 +93,12 @@ $(document).ready(() => {
 
     // left: 37, up: 38, right: 39, down: 40,
     // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-    var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+    var keys = {
+        37: 1,
+        38: 1,
+        39: 1,
+        40: 1
+    };
 
     function preventDefault(e) {
         e.preventDefault();
@@ -87,11 +115,15 @@ $(document).ready(() => {
     var supportsPassive = false;
     try {
         window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-            get: function () { supportsPassive = true; }
+            get: function () {
+                supportsPassive = true;
+            }
         }));
-    } catch (e) { }
+    } catch (e) {}
 
-    var wheelOpt = supportsPassive ? { passive: false } : false;
+    var wheelOpt = supportsPassive ? {
+        passive: false
+    } : false;
     var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
 
     // call this to Disable
@@ -142,15 +174,13 @@ $(document).ready(() => {
 
         navItems.fadeToggle(400, () => {
 
-        }
-        );
+        });
         return !show;
     }
-     $(".box-descrip-short").each(function(i){
-         var len=$(this).text().trim().length;
-         if(len>100)
-         {
-             $(this).text($(this).text().substr(0,100)+'...');
-         }
-     });
-  }); 
+    $(".box-descrip-short").each(function (i) {
+        var len = $(this).text().trim().length;
+        if (len > 100) {
+            $(this).text($(this).text().substr(0, 100) + '...');
+        }
+    });
+});
